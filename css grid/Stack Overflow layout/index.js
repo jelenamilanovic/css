3df375bb2ae00c290a218d0973f1c8795103952f
Jelena Milanovic <jmilanovic114@gmail.com>
2019-01-05 12:09:51 +0100

    Practiced copying a base of Stack Overflow layout

---
+++ css grid/Stack Overflow layout/index.js
@@ -0,0 +1,84 @@
+new Vue({
+  el: '#wrapper',
+  data: {
+    questions: [{
+      title: 'Navigation Bar is not hovering over a mapview swift 4',
+      votes: 0,
+      answers: 0,
+      views: 1,
+      tags: ['ios', 'swift', 'xcode'],
+      lastLog: {
+        time: '18 seconds ago',
+        by: 'Vidit Agrawal'
+      }
+    },
+    {
+      title: 'Rails Admin and dealing with Totals & Subtotals',
+      votes: 0,
+      answers: 0,
+      views: 4,
+      tags: ['ruby-on-rails', 'rails-admin'],
+      lastLog: {
+        time: '33 seconds ago',
+        by: 'The Gugaru'
+      }
+    },
+    {
+      title: 'How do I store multiple options for a Chrome extension?',
+      votes: 0,
+      answers: 2,
+      views: 652,
+      tags: ['javascript', 'google-chrome-extension', 'json'],
+      lastLog: {
+        time: '2 minutes ago',
+        by: 'lksksl'
+      }
+    },
+
+    {
+      title: 'Calling startActivity() from outside of an Activity?',
+      votes: 164,
+      answers: 8,
+      views: 1620,
+      tags: ['android', 'runtimeexception', 'alarmmanager'],
+      lastLog: {
+        time: '2 minutes ago',
+        by: 'Vishrut Mavani'
+      }
+    },
+    {
+      title: 'Calling promise method with recursion is going for infinite',
+      votes: 0,
+      answers: 0,
+      views: 7,
+      tags: ['javascript', 'promise'],
+      lastLog: {
+        time: '2 minutes ago',
+        by: 'Developer'
+      }
+    },
+    {
+      title: 'Sticky Header Jumps up to Top of Screen in Mobile Safari',
+      votes: 1,
+      answers: 0,
+      views: 8,
+      tags: ['javascript', 'jQuery', 'mobile-safari', 'html', 'viewport'],
+      lastLog: {
+        time: '6 minutes ago',
+        by: 'timi95'
+      }
+    },
+    {
+      title: 'Just wondering is there any convention when coding Controller/Model in Laravel?',
+      votes: 2,
+      answers: 0,
+      views: 11,
+      tags: ['php'],
+      lastLog: {
+        time: '6 minutes ago',
+        by: 'GatesPlan'
+      }
+    }
+    ]
+  }
+})
